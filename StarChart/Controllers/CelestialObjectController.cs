7a7d5178d78bee6a83ec4e35a8ced02f21cd6f61
HMBNET\hbarnes <hbarnes@hmbnet.com>
2019-01-10 14:50:59 -0500

    updated http attribute

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -122,7 +122,7 @@ namespace StarChart.Controllers
 
         }
 
-        [HttpPut("{id:int}")]
+        [HttpPut("{id}")]
         public IActionResult Update(int id, CelestialObject celestialObject)
         {
             var existingObject = _context.CelestialObjects.Find(id == celestialObject.Id);
